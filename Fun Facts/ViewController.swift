ec7002868db5ffe1509a2fdb879542f93a0b00de
Jevin Sidhu <jevin.sidhu@gmail.com>
2015-07-27 13:52:28 -0700

    First commit

---
+++ Fun Facts/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Fun Facts
+//
+//  Created by Jevin Sidhu on 2015-06-12.
+//  Copyright (c) 2015 Jevin Sidhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFunFact(sender: AnyObject) {
+        var randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
+
+}
